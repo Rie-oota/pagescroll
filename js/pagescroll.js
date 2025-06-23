2ecffac7618fb4083710402c2ea2dffc0db9f573
Yoshida <sunnri257@gmail.com>
2025-06-23 20:43:50 +0900

    修正

--- js/pagescroll.js
+++ js/pagescroll.js
@@ -4,7 +4,7 @@ $(function () {
     // リンク先の中身を取得する
     const href = $(this).attr("href");
     // リンク先のidがある要素を探して、移動先を取得
-    const target = $(href == "#" || href == "" ? "html" : href);
+    const target = $(href === "#" || href === "" ? "html" : href);
     // ページ上部から移動先までの距離を取得
     const position = target.offset().top;
     // スクロールが完了するまでの時間を5秒に設定
@@ -14,6 +14,5 @@ $(function () {
       // potitonまでspeedの時間をかけて変速でスクロールする
       scrollTop: position
     }, speed, "swing");
-    return false;
   });
 });
